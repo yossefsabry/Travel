69bb78a38e8babe76d1b7c93db67ca70df94b9b4
yossefsabry <yossefsabry66@gmail.com>
2023-07-22 02:50:30 +0300

    -

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# travel
+<h1 align="center"> Travel </h1>
+
+### full website respensive and interactive using html css js
+
+![Full website](./images/view.png)
+
+---
+
+---
+
+---
+
+### My Email : yossefsabry66@gmail.com
--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ bars.addEventListener("click", () => {
     menu.style.display = "flex";
   }
 });
+
+
+
+// scroll button in 30
+
+const scrollTopBtn = document.querySelector('#scroll-top-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 280) {
+    scrollTopBtn.style.display = 'block'; // Show the button when scrolled down by 30 pixels
+  } else {
+    scrollTopBtn.style.display = 'none'; // Hide the button otherwise
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Add smooth scrolling animation
+  });
+});
